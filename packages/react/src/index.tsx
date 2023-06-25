33c139e96aaff9460234feeda11c264beea8096b
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2023-06-25 01:00:43 -0300

    :sparkles: feat: Primeiro storie

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,9 +1,33 @@
+import { ComponentProps } from 'react'
 import { styled } from './styles'
 
-const Button = styled('button', {
-    width: '$10',
-    height: '$3',
-    color: '$ignite500'
+export type ButtonProps = ComponentProps<typeof Button>
+
+export const Button = styled('button', {
+    background: '$ignite500',
+    borderRadius: '$md',
+    color: '$white',
+    display: 'flex',
+    border: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    variants: {
+        size: {
+            small: {
+                fontSize: 14,
+                padding: '$2 $4',
+            },
+            big: {
+                fontSize: 16,
+                padding: '$3 $6',
+            }
+        }
+    },
+
+    defaultVariants: {
+        size: 'small'
+    }
 })
 
 export function App(){

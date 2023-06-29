97d5135a98a4ff23964489747392ed61d756415f
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2023-06-28 23:21:31 -0300

    :sparkles: feat: Stories do componente de Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,19 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+    title: 'Typography/Text',
+    component: Text,
+    args: {
+        children: "Hello world"
+    }
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const Custom: StoryObj<TextProps> = {
+    args: {
+        children: "Strong Text",
+        as: 'strong'
+    }
+}
--- packages/react/dist/index.mjs
+++ packages/react/dist/index.mjs
@@ -118,7 +118,7 @@ var {
   }
 });
 
-// src/components/Box.ts
+// src/components/Box.tsx
 var Box = styled("div", {
   padding: "$4",
   borderRadius: "$md",
@@ -126,6 +126,32 @@ var Box = styled("div", {
   backgroundColor: "$gray800",
   border: "1px solid $gray600"
 });
+
+// src/components/Text.tsx
+var Text = styled("p", {
+  margin: 0,
+  fontFamily: "$default",
+  lineHeight: "$base",
+  color: "$gray100",
+  variants: {
+    size: {
+      xxs: { fontSize: "$xxs" },
+      xs: { fontSize: "$xs" },
+      sm: { fontSize: "$sm" },
+      md: { fontSize: "$md" },
+      lg: { fontSize: "$lg" },
+      xl: { fontSize: "$xl" },
+      "2xl": { fontSize: "$2xl" },
+      "4xl": { fontSize: "$4xl" },
+      "5xl": { fontSize: "$5xl" },
+      "6xl": { fontSize: "$6xl" },
+      "7xl": { fontSize: "$7xl" },
+      "8xl": { fontSize: "$8xl" },
+      "9xl": { fontSize: "$9xl" }
+    }
+  }
+});
 export {
-  Box
+  Box,
+  Text
 };

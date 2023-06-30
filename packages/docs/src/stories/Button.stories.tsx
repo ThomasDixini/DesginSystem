25dc5bf070420ed62db0c4803dcfbe0e44636b37
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2023-06-29 21:20:06 -0300

    chore: ←

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,27 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Button, ButtonProps } from '@ignite-ui/react'
+
+export default {
+    title: 'Form/Button',
+    component: Button,
+    args: {
+        children: "Send"
+    }
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Secondary: StoryObj<ButtonProps> = {
+    args: {
+        variant: 'secondary',
+        children: 'Create new'
+    }
+}
+
+export const Tertiary: StoryObj<ButtonProps> = {
+    args: {
+        variant: 'tertiary',
+        children: 'Disabled',
+        disabled: true
+    }
+}
